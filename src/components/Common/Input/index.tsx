70ead26a5510f7df6757106ddd5d4690a6bc7a66
CrispiToken <kit06092003@gmail.com>
2021-12-26 01:01:29 +0300

    registration form

--- src/components/Common/Input/index.tsx
+++ src/components/Common/Input/index.tsx
@@ -5,22 +5,24 @@ type InputPropsType = {
   setValue: React.Dispatch<React.SetStateAction<string>>;
   placeHolder: string;
   type: string;
+  title: string;
 };
 
 const Input: React.FC<InputPropsType> = (props) => {
-  const { value, setValue, placeHolder, type } = props;
+  const { value, setValue, placeHolder, type, title } = props;
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   return (
-    <div style={{ fontSize: "24px" }}>
+    <div style={{ fontSize: "24px", display: "flex", margin: "10px 0" }}>
+      <span className="title">{title}</span>
       <input
         placeholder={placeHolder}
         type={type}
         onChange={changeHandler}
-        style={{ fontSize: "24px", marginBottom: "20px" }}
+        style={{ fontSize: "24px", margin: "0 0 0 auto" }}
         value={value}
       />
     </div>
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -41,48 +41,45 @@ const SignUp = () => {
           <div className="reg">registration</div>
         </div>
         <div className="inputFild">
-          <div className="informationBlock">
-            <div className="name">Name</div>
-            <div className="surname">Surname</div>
-            <div className="login">Login</div>
-            <div className="password">Password</div>
-            <div className="password">Repeat password</div>
-          </div>
-          <div className="inputBlock">
-            <Input
-              value={firstName}
-              setValue={setFirstName}
-              placeHolder="Введите имя"
-              type="text"
-            />
-            <Input
-              value={lastName}
-              setValue={setLastName}
-              placeHolder="Введите фамилию"
-              type="text"
-            />
-            <Input
-              value={email}
-              setValue={setEmail}
-              placeHolder="Введите email"
-              type="text"
-            />
-            <Input
-              value={password}
-              setValue={setPassword}
-              placeHolder="Введите пароль"
-              type="password"
-            />
-            <Input
-              value={repeatPassword}
-              setValue={setRepeatPassword}
-              placeHolder="Повторите пароль"
-              type="password"
-            />
-          </div>
+          <Input
+            value={firstName}
+            setValue={setFirstName}
+            placeHolder="Введите имя"
+            type="text"
+            title="Name"
+          />
+          <Input
+            value={lastName}
+            setValue={setLastName}
+            placeHolder="Введите фамилию"
+            type="text"
+            title="Surname"
+          />
+          <Input
+            value={email}
+            setValue={setEmail}
+            placeHolder="Введите email"
+            type="text"
+            title="Email"
+          />
+          <Input
+            value={password}
+            setValue={setPassword}
+            placeHolder="Введите пароль"
+            type="password"
+            title="Password"
+          />
+          <Input
+            value={repeatPassword}
+            setValue={setRepeatPassword}
+            placeHolder="Повторите пароль"
+            type="password"
+            title="Repeat password"
+          />
+          {/* </div> */}
         </div>
 
-        <button onClick={handler} type="button">
+        <button onClick={handler} className="btn">
           Sign up
         </button>
       </div>
